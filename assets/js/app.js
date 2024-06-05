60fdc77444bd0a047d6c4279efb93cc2fbcd46ce
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-06-05 15:45:06 +0530

    solve spelling mistakes

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,3 +39,77 @@ document.addEventListener("DOMContentLoaded", function () {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
   });
 });
+
+// COUNTDOWN JS CODE HERE
+     const countDownDate = new Date("Jun 6, 2025 15:37:25").getTime();
+     if (document.querySelector(".countdown")) {
+       const countdown = document.querySelector(".countdown");
+       const hourscontainer = countdown.querySelector(".js-hours span");
+       const minutescontainer = countdown.querySelector(".js-minutes span");
+       const secondscontainer = countdown.querySelector(".js-seconds span");
+
+       const startCountdown = () => {
+         const timer = setInterval(function () {
+           // Get today's date and time
+           let now = new Date().getTime();
+
+           // Find the distance between now and the count down date
+           let distance = countDownDate - now;
+
+           if (distance < 0) {
+             countdown.remove();
+             clearInterval(timer);
+           }
+
+           // Time calculations for days, hours, minutes and seconds
+           let hours = Math.floor(
+             (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+           );
+           let minutes = Math.floor(
+             (distance % (1000 * 60 * 60)) / (1000 * 60)
+           );
+           let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+           // Add leading zeros
+           hours = hours < 10 ? "0" + hours : hours;
+           minutes = minutes < 10 ? "0" + minutes : minutes;
+           seconds = seconds < 10 ? "0" + seconds : seconds;
+
+           // Display the result in the element with id="demo"
+           hourscontainer.innerHTML = hours;
+           minutescontainer.innerHTML = minutes;
+           secondscontainer.innerHTML = seconds;
+         }, 1000);
+       };
+
+       startCountdown();
+     }
+    //  INTRODUCTION SECTION TABS START HERE
+     function myTabs(a) {
+       let tabs = document.querySelectorAll(".tab");
+       let myTabs = [
+         document.getElementById("myTab1"),
+         document.getElementById("myTab2"),
+         document.getElementById("myTab3"),
+         document.getElementById("myTab4"),
+         document.getElementById("myTab5"),
+         document.getElementById("myTab6"),
+         document.getElementById("myTab7"),
+         document.getElementById("myTab8"),
+       ];
+
+       tabs.forEach((tab) => {
+         tab.style.display = "none";
+       });
+
+       myTabs.forEach((tab) => {
+         tab.classList.remove("active");
+       });
+
+       document.getElementById("tab" + a).style.display = "block"; // changed "Tab" to "tab"
+       document.getElementById("myTab" + a).classList.add("active");
+     }
+
+     document.addEventListener("DOMContentLoaded", (event) => {
+       myTabs(1);
+     });
